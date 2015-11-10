455c6ee4c99bd40df0d9545a03479fcfeebf7954
rellbows <ryan.ellis927@gmail.com>
2015-11-09 20:07:18 -0600

    4X4 grid using jquery/table added to etch-a-sketch.js

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,5 +1,25 @@
-$(document).ready(function() {
+//jQuery to create 4X4 grid using CSS
+
+/* $(document).ready(function() {
 	for (var i = 0; i < 16; i++) {
 		$('div#container').append('<div class="square"></div>');
 	}
-});
+}); */
+
+// jQuery to create 4X4 grid using a html table
+
+$(document).ready(function() {
+
+	//make the row
+
+	for (i = 0; i < 4; i++) {
+		var $row = $('<tr>');
+		$('div#container').append($row);
+
+			//make the cells
+
+			for(x = 0; x < 4; x++) {
+				$row.append($('<td class="square">'));
+			}
+	}
+})
