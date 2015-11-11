2a65cbb1aaf88a02d0ba19d404a4f2d475cf1391
rellbows <ryan.ellis927@gmail.com>
2015-11-10 19:14:28 -0600

    added 'hover' effect to .js file using mouseenter/mouseleave. added .hover class to .css file to support this.

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -10,16 +10,29 @@
 
 $(document).ready(function() {
 
+	//Make 4X4 grid
 	//make the row
 
 	for (i = 0; i < 4; i++) {
 		var $row = $('<tr>');
-		$('div#container').append($row);
+		// $('div#container').append('<tr>'); gives a much larger grid. Why? See second loop.
+		$('div#container').append($row); //works
 
 			//make the cells
 
 			for(x = 0; x < 4; x++) {
-				$row.append($('<td class="square">'));
+				// $('tr').append($('<td class="square">')); 
+				$($row).append($('<td class="square">'));
 			}
 	}
+
+	//Mouse hover
+	$('td.square').mouseenter(function() {
+		$(this).addClass('hover');
+	})
+
+	$('td.square').mouseleave(function() {
+		$(this).removeClass('hover');
+	})
+
 })
