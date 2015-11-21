1b0874fe5c93a94441fac1efdd3014395e9cfd00
rellbows <ryan.ellis927@gmail.com>
2015-11-21 12:43:14 -0600

    reset button with prompt for customized grid setup. more trouble with number of cells per row (see notes).

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -10,28 +10,71 @@
 
 $(document).ready(function() {
 
-	//Make 4X4 grid
+	initial = true;
+
+	//Make 16X16 grid
 	//make the row
 
-	for (i = 0; i < 16; i++) {
-		var $row = $('<tr>');
-		// $('div#container').append('<tr>'); gives a much larger grid. Why? See second loop.
-		$('table').append($row); //works
 
-			//make the cells
+	if (initial) {
+		for (var i = 0; i < 16; i++) {
+			$row = $('<tr></tr>');
+			// $('div#container').append('<tr>'); gives a much larger grid. Why? See second loop.
+			$('tbody').append($row); //works
 
-			for(x = 0; x < 16; x++) {
-				// $('tr').append($('<td class="square">')); 
-				$($row).append($('<td class="square">'));
-			}
+				//make the cells
+
+				for(var x = 0; x < 16; x++) {
+					// $('tr').append($('<td class="square">')); 
+					$($row).append($('<td class="square">'));
+				}
+		}
+		initial = false;
 	}
-	//Mouse hover
-	$('td.square').mouseenter(function() {
-		$(this).addClass('hover');
-	})
+
+	mouseHover();
+
+	shake();
+/* Moved out of inital page loading
 
 	$('#reset').click(function() {
 		$('td.square.hover').removeClass('hover');
-	})
+		var grid = prompt("How detailed of a square grid do you want?");
+		console.log(grid); //made it this far...
+		for (w = 0; w < grid; w++) {
+			console.log(x);
+			$('table').append($row);
+				for(j = 0; j < grid; j++)
+					$($row).append($('<td class="square">'));
+		}
+	});*/
 
 });
+
+function mouseHover() {
+		$('td.square').mouseenter(function() {
+		$(this).addClass('hover');
+		})
+};
+
+function shake() {
+	$('#reset').click(function() {
+		$('tr').remove();
+		var grid = prompt("How detailed of a square grid do you want?");
+		console.log(grid); //made it this far...
+		
+
+		for (var w = 0; w < grid; w++) {
+			console.log(w + "row"); //made it here...
+			$('tbody').append('<tr />');
+
+
+				for(var j = 0; j < grid; j++) {
+					console.log(j + "cell");
+					$('tr').append('<td class="square"></td>');
+				}
+		}
+
+		mouseHover();		
+	})
+};
